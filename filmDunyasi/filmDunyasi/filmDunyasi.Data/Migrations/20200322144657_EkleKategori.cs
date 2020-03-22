aa5aa6ee20c2aee9814525ce60e4e9f6bad4583a
Serdar KOCER <serdarkocerr@gmail.com>
2020-03-22 19:22:25 +0300

    hafta7 islendi. Kategori Controller oluşturuldu.

---
+++ filmDunyasi/filmDunyasi/filmDunyasi.Data/Migrations/20200322144657_EkleKategori.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace filmDunyasi.Data.Migrations
+{
+    public partial class EkleKategori : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kategoris",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tur = table.Column<string>(nullable: true),
+                    Sira = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategoris", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategoris");
+        }
+    }
+}
---
+++ filmDunyasi/filmDunyasi/filmDunyasi.Web/Controllers/KategoriController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using filmDunyasi.Data;
+using filmDunyasi.Models;
+
+namespace filmDunyasi.Web.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KategoriController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Kategori
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Kategoris.ToListAsync());
+        }
+
+        // GET: Kategori/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kategori = await _context.Kategoris
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (kategori == null)
+            {
+                return NotFound();
+            }
+
+            return View(kategori);
+        }
+
+        // GET: Kategori/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kategori/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Tur,Sira")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(kategori);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kategori);
+        }
+
+        // GET: Kategori/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kategori = await _context.Kategoris.FindAsync(id);
+            if (kategori == null)
+            {
+                return NotFound();
+            }
+            return View(kategori);
+        }
+
+        // POST: Kategori/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Tur,Sira")] Kategori kategori)
+        {
+            if (id != kategori.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kategori);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KategoriExists(kategori.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kategori);
+        }
+
+        // GET: Kategori/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kategori = await _context.Kategoris
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (kategori == null)
+            {
+                return NotFound();
+            }
+
+            return View(kategori);
+        }
+
+        // POST: Kategori/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var kategori = await _context.Kategoris.FindAsync(id);
+            _context.Kategoris.Remove(kategori);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KategoriExists(int id)
+        {
+            return _context.Kategoris.Any(e => e.ID == id);
+        }
+    }
+}
