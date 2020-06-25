3a038c563e46708d1902c6bfdba268e64b85fa50
Serdar KOCER <serdarkocerr@gmail.com>
2020-06-25 07:48:06 +0300

    IdentityUserLogin primary key hata cozumu koyuldu

--- filmDunyasi/filmDunyasi/filmDunyasi.Data/ApplicationDbContext.cs
+++ filmDunyasi/filmDunyasi/filmDunyasi.Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using filmDunyasi.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,23 @@ namespace filmDunyasi.Data
         {
         }
 
+        /*
+         *The entity type 'IdentityUserLogin' requires a primary key to be defined. 
+         * If you intended to use a keyless entity type call 'HasNoKey()'.
+         * hatasının cozumu asagıdakı methodu override edip Ignore'lari belirlemek
+         * olarak gosteriliyor. 
+         * NEDENI TAM OLARAK ARASTIRILMALI
+         * 
+         * protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             base.OnModelCreating(modelBuilder);
+             modelBuilder.Ignore<IdentityUserLogin<string>>();
+             modelBuilder.Ignore<IdentityUserRole<string>>();
+             modelBuilder.Ignore<IdentityUserClaim<string>>();
+             modelBuilder.Ignore<IdentityUserToken<string>>();
+             modelBuilder.Ignore<IdentityUser<string>>();
+             modelBuilder.Ignore<ApplicationUser>();
+         }*/
         public DbSet<Kategori> Kategoris { get; set; }
         public DbSet<Ulke> Ulke { get; set; }
 
